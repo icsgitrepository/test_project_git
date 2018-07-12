a365fab802d330c954e713895da20e738f92315d
arivas <arivas@192.168.15.6>
2018-07-12 16:51:23 -0500

    Segunda modificación desde México

--- Prueba_GIT/src/prueba_git/Prueba_GIT.java
+++ Prueba_GIT/src/prueba_git/Prueba_GIT.java
@@ -16,6 +16,7 @@ public class Prueba_GIT {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo, desde México");
+        System.out.println("Hola mundo, segundo mensaje desde México");
     }
     
 }
